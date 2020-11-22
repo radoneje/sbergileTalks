a018ffbd97cb4a11520b983f7e7c4d3ae861a96f
radoneje <den.shevchenko@gmail.com>
2020-11-22 22:19:12 +0300

    efvfe

--- app.js
+++ app.js
@@ -41,12 +41,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", (req,res, next)=>{req.knex=knex;next();});
